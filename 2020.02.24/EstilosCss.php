63d535bd56c102803eb68041e85065df44981e05
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2020-02-24 17:54:50 -0600

    Ejercicio 1: Estilos CSS
    En este ejercicio vimos como utilizar estilos CSS dentro del mismo archivo declarando los mismos dentro de la etiqueta <style></style>

---
+++ 2020.02.24/EstilosCss.php
@@ -0,0 +1,28 @@
+<html>
+  <head>
+    <style type="text/css">
+    estilo{
+      color:#009900;
+      font-family: verdana;
+      font-size: 25px;
+    }
+      p{
+        color:white;
+        font-family: verdana;
+        font-size: 25px;
+        background-color: #999999;
+      }
+      estiloa{
+        color:#ff0000;
+        font-family: verdana;
+        font-size: 25px;
+      }
+
+    </style>
+  </head>
+  <body>
+      <estilo>Esto es un parrafo con estilos css</estilo>
+      <p>Esto es un parrafo con estilos css</p>
+      <estiloa>Esto es un parrafo con estilos css</estiloa>
+  </body>
+</html>
