7696b27fd20b03992df98e5f5a6d31bbbfe683af
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2020-02-25 17:28:38 -0600

    Creación de la pagina y su hoja de estilos

---
+++ 2020.02.25/Ejemplo2u2.php
@@ -0,0 +1,10 @@
+<!Doctype>
+<html>
+  <head>
+    <title>Ejemplo de estilos CSS en un archivo externo </title>
+    <link rel="stylesheet"  type="text/css" href="css/estilos.css"/>
+  </head>
+  <body>
+      <p>Un párrafo de texto. Usando archivos externos</p>
+  </body>
+</html>
