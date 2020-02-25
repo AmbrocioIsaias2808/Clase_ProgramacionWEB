35281089aa6e7e9f45ed200a29d009986d146adc
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2020-02-25 17:50:11 -0600

    Modificaciones

--- 2020.02.25/Ejemplo2u2.php
+++ 2020.02.25/Ejemplo2u2.php
@@ -5,6 +5,8 @@
     <link rel="stylesheet"  type="text/css" href="css/estilos.css"/>
   </head>
   <body>
-      <p>Un párrafo de texto. Usando archivos externos</p>
+    <parrafo1>Un párrafo de texto. Usando archivos externos. Parrafo 1</parrafo1>
+    <p>Un párrafo de texto. Usando archivos externos</p>
+    <parrafo3>Un párrafo de texto. Usando archivos externos. Parrafo 3</parrafo3>
   </body>
 </html>
