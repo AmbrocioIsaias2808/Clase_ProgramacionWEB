4f56c203b006a02d427bc893c6eff9cb04c1640d
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2020-02-27 17:47:53 -0600

    Ejercicio 3 unidad 2

---
+++ 2020.02.27/Ejercicio3u2.php
@@ -0,0 +1,21 @@
+<!Doctype>
+<html>
+  <head>
+    <title>Listas ordenadas y listas desordenadas</title>
+  </head>
+  <body>
+  <p>Procedimiento para ingresar a la configuración</p>
+  <ol>
+    <h2>Ejemplo de lista ordenada</h2>
+    <li>Encienda la computadora</li>
+    <li>Presione la tecla <kbd>DEL</kbd> cuando se le indique</li>
+    <li>Ingrese la contraseña</li>
+  </ol>
+  <ul>
+    <h2>Ejemplo de lista ordenada</h2>
+    <li>Encienda la computadora</li>
+    <li>Presione la tecla <kbd>DEL</kbd> cuando se le indique</li>
+    <li>Ingrese la contraseña</li>
+  </ul>
+  </body>
+</html>
