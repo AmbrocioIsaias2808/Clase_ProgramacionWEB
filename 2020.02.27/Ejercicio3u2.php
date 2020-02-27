b6c505336cfba99305ee772998ff44641a28b0e7
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2020-02-27 17:54:29 -0600

    Agregado de CSS y cambio de letaniad

--- 2020.02.27/Ejercicio3u2.php
+++ 2020.02.27/Ejercicio3u2.php
@@ -1,6 +1,7 @@
 <!Doctype>
 <html>
   <head>
+    <link rel="stylesheet" href="css.css"/>
     <title>Listas ordenadas y listas desordenadas</title>
   </head>
   <body>
@@ -12,7 +13,7 @@
     <li>Ingrese la contraseña</li>
   </ol>
   <ul>
-    <h2>Ejemplo de lista ordenada</h2>
+    <h2>Ejemplo de lista desordenada</h2>
     <li>Encienda la computadora</li>
     <li>Presione la tecla <kbd>DEL</kbd> cuando se le indique</li>
     <li>Ingrese la contraseña</li>
