a1ac9cc7293e75d0473362bcd3d3a18910b13b45
AmbrocioIsaias2808 <44043979+AmbrocioIsaias2808@users.noreply.github.com>
2020-03-10 17:51:38 -0500

    Agregado de mini barra de navegación

---
+++ 2020.03.11/Ejercicio9.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <nav>
+      <div align="left">
+        <ul>
+          <li> <a href="index.html">Inicio</a></li>
+          <li> <a href="nosotros.html">Nosotros</a></li>
+          <li> <a href="divseresc.html">Página Web</a>
+          <ul>
+            <li> <a href="solicitud">Visitantes</a>
+            <ul style="background-color:#97c8b2">
+              <li> <a href="visitante_alta.php">Altas</a> </li>
+              <li> <a href="visitante_bajas.php">Bajas</a> </li>
+              <li> <a href="visitante_bajas.php">Modificaciones</a> </li>
+              <li> <a href="visitante_bajas.php">Consultas</a> </li>
+            </ul>
+          </li>
+            <li> <a href="estatusde.php">Otra opción a</a>
+            <ul style="background-color:#97c8b2">
+              <li> <a href="autotransfusion.html">Otra sub-opcion a</a> </li>
+            </ul>
+            </li>
+          </ul>
+        </li>
+        </ul>
+      </div>
+    </nav>
+  </body>
+</html>
